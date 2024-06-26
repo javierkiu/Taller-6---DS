a81f3adfb2c1696c220647be9f144828d776f480
Javier Rodriguez <81196360+javierkiu@users.noreply.github.com>
2024-06-26 16:24:47 -0500

    Add files via upload

---
+++ taller6/src/main/java/com/mycompany/taller6/Taller6.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.taller6;
+
+/**
+ *
+ * @author CltControl
+ */
+public class Taller6 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
